e1ef8796b2695aeb10acf18ab0d412fada9aa442
Ralph Cacho <cacho.rc@gmail.com>
2019-01-08 20:31:24 -0800

    Add request for weather API

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 const port = 3000
+
+const apiKey = '6ec26a88ac0d8673db9fb923256e0c7e'
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('index', {weather: null, error: null}))
 
 app.post('/', (req, res) => {
-  res.render('index');
-  console.log(req.body.city)
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function(err, response, body) {
+    if(err) {
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined) {
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}.`;
+        res.render('index', {weather: weatherText, error: null})
+      }
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
