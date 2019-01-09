b4aa48a5a2e68cc7e34f906fa27af1a07baef037
Ralph Cacho <cacho.rc@gmail.com>
2019-01-08 19:56:31 -0800

    Add view and styling

--- server.js
+++ server.js
@@ -2,5 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.post('/', (req, res) => {
+  res.render('index');
+})
