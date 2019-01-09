a633891bd78d8a1cb1379e73bd609a4d4f3d3bb4
Ralph Cacho <cacho.rc@gmail.com>
2019-01-08 20:02:02 -0800

    Place post before listen

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 app.post('/', (req, res) => {
   res.render('index');
+  console.log(req.body.city)
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
